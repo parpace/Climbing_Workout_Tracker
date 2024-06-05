0c1045ef92ebdc913a5f128818295de58bd6cb45
parpace <parpace@gmail.com>
2024-06-05 13:42:04 -0500

    Can now exit out of plan, categories, and workouts... Horrible method but nothing else was working

--- client/userPage.js
+++ client/userPage.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    const body = document.querySelector(`.body`)
+    const userProfile = document.querySelector('.userProfile')
+    const calendar = document.getElementById('calendar')
+    const currentMonth = document.getElementById('currentMonth')
+    const prevMonthButton = document.getElementById('prevMonth')
+    const nextMonthButton = document.getElementById('nextMonth')
+    const dayPlan = document.getElementById('dayPlan')
+    const planDate = document.getElementById('planDate')
+    const planContent = document.getElementById('planContent')
+    const addWorkout = document.getElementById('addWorkout')
+    const categoryContainer = document.getElementById('categoryContainer')
+    const categoriesDiv = document.getElementById('categories')
+    const workoutContainer = document.getElementById('workoutContainer')
+    const workoutsDiv = document.getElementById('workouts')
+    const planExit = document.getElementById(`planExit`)
+    const categoryExit = document.getElementById(`categoryExit`)
+    const workoutExit = document.getElementById(`workoutExit`)
+    
     // Get the user ID from the URL query parameter
     const urlParams = new URLSearchParams(window.location.search)
     const userId = urlParams.get('userId')
@@ -20,19 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
             userProfileDiv.innerHTML = `<p>Error loading user data.</p>`
         })
 
-    const userProfile = document.querySelector('.userProfile')
-    const calendar = document.getElementById('calendar')
-    const currentMonth = document.getElementById('currentMonth')
-    const prevMonthButton = document.getElementById('prevMonth')
-    const nextMonthButton = document.getElementById('nextMonth')
-    const dayPlan = document.getElementById('dayPlan')
-    const planDate = document.getElementById('planDate')
-    const planContent = document.getElementById('planContent')
-    const addWorkout = document.getElementById('addWorkout')
-    const categoryContainer = document.getElementById('categoryContainer')
-    const categoriesDiv = document.getElementById('categories')
-    const workoutContainer = document.getElementById('workoutContainer')
-    const workoutsDiv = document.getElementById('workouts')
 
     let currentCalendar = 'planned'
 
@@ -100,6 +106,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function openDayPlan(year, month, day) {
+
+        userProfile.classList.add('dimmed')
         const selectedDate = new Date(year, month, day)
         const formattedDate = selectedDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })
         planDate.textContent = formattedDate
@@ -121,7 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         workoutElement.appendChild(logButton)
 
                         const deleteButton = document.createElement(`button`)
-                        deleteButton.textContent = `x`;
+                        deleteButton.textContent = `Delete`;
                         deleteButton.addEventListener(`click`, () => deleteWorkout(workout._id))
                         workoutElement.appendChild(deleteButton)
 
@@ -215,8 +223,27 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     addWorkout.addEventListener(`click`, () => {
-        console.log('Add workout button clicked')
+        dayPlan.style.display = `none`
         renderCategories()
+    })
+
+    planExit.addEventListener(`click`, () => {
+        dayPlan.style.display = 'none'
+        categoryContainer.style.display = 'none'
+        workoutContainer.style.display = 'none'
+        userProfile.classList.remove('dimmed')
+    })
+    categoryExit.addEventListener(`click`, () => {
+        dayPlan.style.display = 'none'
+        categoryContainer.style.display = 'none'
+        workoutContainer.style.display = 'none'
+        userProfile.classList.remove('dimmed')
+    })
+    workoutExit.addEventListener(`click`, () => {
+        dayPlan.style.display = 'none'
+        categoryContainer.style.display = 'none'
+        workoutContainer.style.display = 'none'
+        userProfile.classList.remove('dimmed')
     })
 
     prevMonthButton.addEventListener('click', function() {
@@ -231,163 +258,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     renderPlanner(currentDate)
 
-//     function fetchAndRenderCategories() {
-//         axios.get('http://localhost:3001/categories')
-//             .then(response => {
-//                 const categories = response.data
-//                 categoriesDiv.innerHTML = ''
-//                 categories.forEach(category => {
-//                     const categoryElement = document.createElement('div')
-//                     categoryElement.textContent = category.name
-//                     categoriesDiv.appendChild(categoryElement)
+    // body.addEventListener('click', function(event) {
+    //     const outsideDayPlan = !dayPlan.contains(event.target)
+    //     const outsideCategoryContainer = !categoryContainer.contains(event.target)
+    //     const outsideWorkoutContainer = !workoutContainer.contains(event.target)
+    //     if (outsideDayPlan && outsideCategoryContainer && outsideWorkoutContainer) {
+    //         userProfile.classList.remove('dimmed')
+    //         dayPlan.style.display = 'none'
+    //     }
+    // })
 
-//                     categoryElement.addEventListener('click', () => {
-//                         fetchAndRenderWorkouts(category._id)
-//                     })
-//                 })
-//                 categoryContainer.style.display = 'flex'
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching categories:', error)
-//             })
-//     }
-
-//     function fetchAndRenderWorkouts(categoryId) {
-//         axios.get(`http://localhost:3001/workouts?category=${categoryId}`)
-//             .then(response => {
-//                 const workouts = response.data
-//                 workoutsDiv.innerHTML = ''
-//                 workouts.forEach(workout => {
-//                     const workoutElement = document.createElement('div')
-//                     workoutElement.textContent = workout.name
-//                     workoutsDiv.appendChild(workoutElement)
-
-//                     workoutElement.addEventListener('click', () => {
-//                         addWorkoutToPlan(workout)
-//                     })
-//                 })
-//                 workoutContainer.style.display = 'flex'
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching workouts:', error)
-//             })
-//     }
-
-//     function addWorkoutToPlan(workout) {
-//         // Add the workout to the plan content
-//         const workoutElement = document.createElement('div')
-//         workoutElement.textContent = workout.name
-//         planContent.appendChild(workoutElement)
-
-//         // Save the workout to the user's plan in the database
-//         axios.post(`http://localhost:3001/user/${userId}/addWorkout`, {
-//             date: planDate.textContent,
-//             workoutId: workout._id
-//         })
-//         .then(response => {
-//             console.log('Workout added to plan:', response.data)
-//         })
-//         .catch(error => {
-//             console.error('Error adding workout to plan:', error)
-//         })
-//     }
-
-//     function fetchAndRenderWorkoutPlan(date, userId) {
-//         const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-//         axios.get(`http://localhost:3001/user/${userId}/workouts?date=${formattedDate}`)
-//             .then(response => {
-//                 const workouts = response.data
-//                 planContent.innerHTML = ''
-//                 workouts.forEach(workout => {
-//                     const workoutElement = document.createElement('div')
-//                     workoutElement.textContent = workout.name
-//                     planContent.appendChild(workoutElement)
-//                 })
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching workout plan:', error)
-//             })
-//     }
-
-//     const outsideDayPlan = !dayPlan.contains(event.target) && event.target !== addWorkout
-//     const outsideCategoryContainer = !categoryContainer.contains(event.target) && event.target !== addWorkout
-//     const outsideWorkoutContainer = !workoutContainer.contains(event.target) && event.target !== addWorkout
-
-//     if (outsideCategoryContainer && outsideDayPlan && outsideWorkoutContainer) {
-//         userProfile.classList.remove('dimmed')
-//         dayPlan.style.display = 'none'
-//         categoryContainer.style.display = 'none'
-//         workoutContainer.style.display = 'none'
-//         if (overlay.parentNode) {
-//             overlay.parentNode.removeChild(overlay)
-//         }
-//     }
 })
 
+// document.addEventListener('click', function(event) {
+    //     const outsideDayPlan = !dayPlan.contains(event.target) && event.target !== addWorkout
+    //     const outsideCategoryContainer = !categoryContainer.contains(event.target)
+    //     const outsideWorkoutContainer = !workoutContainer.contains(event.target)
 
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const calendar = document.getElementById('calendar')
-//     const currentMonth = document.getElementById('currentMonth')
-//     const prevMonthButton = document.getElementById('prevMonth')
-//     const nextMonthButton = document.getElementById('nextMonth')
-
-//     let currentDate = new Date()
-//     const months = [
-//         'January', 'February', 'March', 'April', 'May', 'June',
-//         'July', 'August', 'September', 'October', 'November', 'December'
-//     ]
-
-//     function renderCalendar(date) {
-//         calendar.innerHTML = ''
-//         const month = date.getMonth()
-//         const year = date.getFullYear()
-//         currentMonth.textContent = `${months[month]} ${year}`
-
-//         const firstDayOfMonth = new Date(year, month, 1).getDay()
-//         const daysInMonth = new Date(year, month + 1, 0).getDate()
-
-//         // Fill in the days of the week headers
-//         const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-//         daysOfWeek.forEach(day => {
-//             const dayElement = document.createElement('div')
-//             dayElement.textContent = day
-//             dayElement.style.fontWeight = 'bold'
-//             calendar.appendChild(dayElement)
-//         })
-
-//         // Fill in the blank days before the start of the month
-//         for (let i = 0; i < firstDayOfMonth; i++) {
-//             const blankDay = document.createElement('div')
-//             calendar.appendChild(blankDay)
-//         }
-
-//         // Fill in the days of the month
-//         for (let day = 1; day <= daysInMonth; day++) {
-//             const dayElement = document.createElement('div')
-//             dayElement.textContent = day
-//             calendar.appendChild(dayElement)
-
-//             dayElement.addEventListener('click', () => {
-//                 fetchAndRenderWorkoutPlan(category._id)
-//             })
-//         }
-//     }
-
-//     prevMonthButton.addEventListener('click', function() {
-//         currentDate.setMonth(currentDate.getMonth() - 1)
-//         renderCalendar(currentDate)
-//     })
-
-//     nextMonthButton.addEventListener('click', function() {
-//         currentDate.setMonth(currentDate.getMonth() + 1)
-//         renderCalendar(currentDate)
-//     })
-
-//     renderCalendar(currentDate)
-// })
-
+    //     if (outsideCategoryContainer && outsideDayPlan && outsideWorkoutContainer) {
+    //         userProfile.classList.remove('dimmed')
+    //         dayPlan.style.display = 'none'
+    //         categoryContainer.style.display = 'none'
+    //         workoutContainer.style.display = 'none'
+    //         if (overlay.parentNode) {
+    //             overlay.parentNode.removeChild(overlay)
+    //         }
+    //     }
+    // })
 
 // document.addEventListener('click', function(event) {
 //     const outsideCategoryContainer = !categoryContainer.contains(event.target) && event.target !== addWorkout
@@ -404,9 +301,7 @@ document.addEventListener('DOMContentLoaded', function() {
 // })
 
 
-// const addWorkout = document.querySelector('#add')
-// const categories = document.querySelector('.categories')
-// const userProfile = document.querySelector('.userProfile')
+
 // const overlay = document.createElement('div')
 
 // overlay.className = 'overlay'
@@ -431,33 +326,3 @@ document.addEventListener('DOMContentLoaded', function() {
 //         })
 //     })
 // })
-
-// async function fetchAndRenderSubcategories(categoryId) {
-//     try{
-//         let subcategoryResponse = await axios.get(`http://localhost:3001/subcategories`)
-//         console.log(subcategoryResponse.data)
-//         const subcategoryArray = subcategoryResponse.data
-        
-//         const filteredSubcategories = subcategoryArray.filter(subcategory => subcategory.category_id === categoryId)
-//         console.log('Filtered Subcategories:', filteredSubcategories)
-
-//         subcategories.innerHTML = ''
-
-//         filteredSubcategories.forEach(subcategory => {
-//                 const subcategoryElement = document.createElement('div')
-//                 subcategoryElement.textContent = subcategory.name
-//                 subcategories.appendChild(subcategoryElement)
-//         })
-
-//         userProfile.classList.add('dimmed')
-//         document.body.appendChild(overlay)
-//         categoryContainer.style.display = `none`
-//         subcategoryContainer.style.display = 'flex'
-
-//         if (filteredSubcategories.length === 0) {
-//             console.log('No subcategories found for category ID:', categoryId);
-//         }
-//     } catch (error) {
-//         console.error('Error fetching subcategories:', error);
-//     }
-// }
